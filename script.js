bb9353f1407f51eb7d2535d98aab66df42d9446b
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-03-22 10:32:30 +0300

    adding a node functioon

---
+++ script.js
@@ -0,0 +1,16 @@
+const slides=documennt.querySelectorAll(".slides img");
+let slideindex = 0;
+let intervalId = null;
+initializeSlidder()
+function initializeSlidder(){
+slides[slideindex].classList.add("displaySlide")
+}
+function showSlide(){
+
+}
+function prevSlide(){
+    
+}
+function nextSlide(){
+    
+}
